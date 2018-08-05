9485e08c1e0b21f7caf946d060e619cb691afb2a
Yordan432 <dani.583@abv.bg>
2018-08-05 21:25:52 +0300

    binary tree

--- Data Strucure/05.Basic Tree/BinaryTree/BinaryTree.cs
+++ Data Strucure/05.Basic Tree/BinaryTree/BinaryTree.cs
@@ -1,6 +1,67 @@
 ﻿
+using System;
+using System.Collections.Generic;
+
 public class BinaryTree<T>
 {
+    public T Root { get; set; }
+    public BinaryTree<T> LeftChild { get; set; }
+    public BinaryTree<T> RigthChild { get; set; }
 
+
+    public BinaryTree(T value, 
+        BinaryTree<T> leftChild = null, 
+        BinaryTree<T> rightChild = null)
+    {
+        this.Root = value;
+        this.LeftChild = leftChild;
+        this.RigthChild = rightChild;
+    }
+
+    public void InOrder(Action<T> action)
+    {
+        Each(action, this);
+    }
+
+    private void Each(Action<T> action, BinaryTree<T> tree)
+    {
+        if (tree == null)
+        {
+            return;
+        }
+        Each(action, tree.LeftChild);
+        action(tree.Root);
+        Each(action, tree.RigthChild);
+    }
+    
+    public void PreOrder(int intervals = 0)
+    {
+        Console.WriteLine($"{new string(' ', intervals)}{this.Root}");
+
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.PreOrder(intervals + 2);
+        }
+
+        if (this.RigthChild != null)
+        {
+            this.RigthChild.PreOrder(intervals + 2);
+        }
+    }
+
+    public void PostOrder(Action<T> action)
+    {
+        PostOrderTraversal(action, this);
+    }
+    private void PostOrderTraversal(Action<T> action, BinaryTree<T> tree)
+    {
+        if (tree == null)
+        {
+            return;
+        }
+        Each(action, tree.LeftChild);
+        Each(action, tree.RigthChild);
+        action(tree.Root);
+    }
 }
 
--- Data Strucure/05.Basic Tree/BinaryTree/StartUp.cs
+++ Data Strucure/05.Basic Tree/BinaryTree/StartUp.cs
@@ -7,7 +7,22 @@ namespace BinaryTree
     {
         static void Main()
         {
+            var binaryTree = new BinaryTree<int>(5,
+                new BinaryTree<int>(4),
+                new BinaryTree<int>(9));
 
+            Console.WriteLine("Pre-order traversal: ");
+            binaryTree.PreOrder();
+
+            Console.WriteLine();
+
+            Console.WriteLine("In-order traversal: ");
+            binaryTree.InOrder(x => Console.WriteLine(x));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Post-order traversal: ");
+            binaryTree.PostOrder(x => Console.WriteLine(x));
         }
     }
 }
