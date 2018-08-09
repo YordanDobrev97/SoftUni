b9b237d61499ffa7b74b6641d121ec177463f7f8
Yordan432 <dani.583@abv.bg>
2018-08-09 15:00:57 +0300

    last 2 problem from Basic Tree

---
+++ Data Strucure/06.Basic Tree Data Structure - Exercises/09.AllSubtreesWithGivenSum/AllSubtreesWithGivenSum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using _01.Introduction;
+
+namespace _09.AllSubtreesWithGivenSum
+{
+    class AllSubtreesWithGivenSum
+    {
+        static void Main()
+        {
+            Introduction.CreateTree();
+            Introduction.AllSubtreesGiveSum();
+        }
+    }
+}
