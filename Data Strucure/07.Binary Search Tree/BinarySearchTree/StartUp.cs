292eb6e9f2d66796b689fd212e2119485cfc3953
Yordan432 <dani.583@abv.bg>
2018-08-10 21:31:52 +0300

    Binary Search Tree

---
+++ Data Strucure/07.Binary Search Tree/BinarySearchTree/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var bst = new BinarySearchTree<int>();
+            bst.Insert(10);
+            bst.Insert(37);
+            bst.Insert(1);
+            bst.Insert(5);
+            bst.Insert(3);
+
+            var smallBinarySearch = bst.Search(1);
+        }
+    }
+}
