ef9a057cadab7166b627f620ab87965954d9d159
Yordan432 <dani.583@abv.bg>
2018-08-11 22:03:19 +0300

    Programming Basic Exam 16 June 2018
    520/600

---
+++ Programming Basics with C# - September 2018/14.Exams/Programming Basics 16 June 2018 Exam/03.Cat Life/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace _03.Cat_Life
+{
+    class Program
+    {
+        static void Main()
+        {
+            string breed = Console.ReadLine();
+            char genderCat = char.Parse(Console.ReadLine());
+
+            int yearsCat = 0;
+            switch (breed)
+            {
+                case "British Shorthair":
+                    if (genderCat == 'm')
+                    {
+                        yearsCat = 13;
+                    }
+                    else
+                    {
+                        yearsCat = 14;
+                    }
+                    break;
+                case "Siamese":
+                    if (genderCat == 'm')
+                    {
+                        yearsCat = 15;
+                    }
+                    else
+                    {
+                        yearsCat = 16;
+                    }
+                    break;
+                case "Persian":
+                    if (genderCat == 'm')
+                    {
+                        yearsCat = 14;
+                    }
+                    else
+                    {
+                        yearsCat = 15;
+                    }
+                    break;
+                case "Ragdoll":
+                    if (genderCat == 'm')
+                    {
+                        yearsCat = 16;
+                    }
+                    else
+                    {
+                        yearsCat = 17;
+                    }
+                    break;
+                case "American Shorthair":
+                    if (genderCat == 'm')
+                    {
+                        yearsCat = 12;
+                    }
+                    else
+                    {
+                        yearsCat = 13;
+                    }
+                    break;
+                case "Siberian":
+                    if (genderCat == 'm')
+                    {
+                        yearsCat = 11;
+                    }
+                    else
+                    {
+                        yearsCat = 12;
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"{breed} is invalid cat!");
+                    return;
+            }
+            Console.WriteLine($"{yearsCat * 12 / 6} cat months");
+        }
+    }
+}
---
+++ Programming Basics with C# - September 2018/14.Exams/Programming Basics 16 June 2018 Exam/05.Cat Watch/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _05.Cat_Watch
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Basic(n);
+
+            Middle(n);
+
+            Basic(n);
+        }
+
+        private static void Middle(int n)
+        {
+            Console.Write(new string(' ', n - 1));
+            Console.Write("//");
+            Console.Write(new string(' ', n));
+            Console.WriteLine("\\\\");
+
+            int specialRowSymbol = (n - 4) / 3;
+            for (int i = 0; i < n - 4; i++)
+            {
+                Console.Write(new string(' ', n - 2));
+                Console.Write("||");
+                Console.Write(new string(' ', n + 2));
+                Console.Write("||");
+                if (i == specialRowSymbol)
+                {
+                    Console.WriteLine("]");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            Console.Write(new string(' ', n - 1));
+            Console.Write("\\\\");
+            Console.Write(new string(' ', n));
+            Console.WriteLine("//");
+        }
+
+        private static void Basic(int n)
+        {
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write(new string(' ', n));
+                Console.Write("||");
+                Console.Write(new string('_', n - 2));
+                Console.WriteLine("||");
+            }
+        }
+    }
+}
