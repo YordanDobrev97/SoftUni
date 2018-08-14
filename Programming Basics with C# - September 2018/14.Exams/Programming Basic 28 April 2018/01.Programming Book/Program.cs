ad485d4078e09c54af202b7cf6463af5ab64e526
Yordan432 <dani.583@abv.bg>
2018-08-14 13:32:16 +0300

    Programming Basic 28 April 600/600
    600/600 solved

---
+++ Programming Basics with C# - September 2018/14.Exams/Programming Basic 28 April 2018/01.Programming Book/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.Programming_Book
+{
+    class Program
+    {
+        static void Main()
+        {
+            double pricePage = double.Parse(Console.ReadLine());
+            double priceCover = double.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+            double sumPerDesigner = double.Parse(Console.ReadLine());
+            double totalDueAmount = double.Parse(Console.ReadLine());
+
+            double price = pricePage * 899 + priceCover * 2;
+            price = price - (price * (discount / 100.0));
+            
+            price += sumPerDesigner;
+            price = price - (price * (totalDueAmount / 100.0));
+
+            Console.WriteLine($"Avtonom should pay {price:f2} BGN.");
+        }
+    }
+}
---
+++ Programming Basics with C# - September 2018/14.Exams/Programming Basic 28 April 2018/04.Credit System/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _04.Credit_System
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberCoursesRecord = int.Parse(Console.ReadLine());
+
+            double percent = 0;
+            double averageGrade = 0.0;
+            double numberCredit = 0.0;
+            for (int i = 0; i < numberCoursesRecord; i++)
+            {
+                int digits = int.Parse(Console.ReadLine());
+                int grade = digits % 10;
+                averageGrade += grade;
+                digits /= 10;
+                int numberCredits = digits;
+
+                if (grade == 2)
+                {
+                    percent = 0;
+                }
+                else if (grade == 3)
+                {
+                    percent = numberCredits * 0.50;
+                }
+                else if(grade == 4)
+                {
+                    percent = numberCredits * 0.70;
+                }
+                else if(grade == 5)
+                {
+                    percent = numberCredits * 0.85;
+                }
+                else if(grade == 6)
+                {
+                    percent = numberCredits;
+                }
+                numberCredit += percent;
+            }
+            Console.WriteLine($"{numberCredit:f2}");
+            Console.WriteLine($"{averageGrade / numberCoursesRecord:f2}");
+        }
+    }
+}
