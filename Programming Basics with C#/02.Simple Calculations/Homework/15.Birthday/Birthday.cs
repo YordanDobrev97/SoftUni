02e6e63652be1b7f1474e9e704e9f56f63f632cc
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2018-09-08 17:09:00 +0300

    add new task - birthday

---
+++ Programming Basics with C#/02.Simple Calculations/Homework/15.Birthday/Birthday.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _15.Birthday
+{
+    class Birthday
+    {
+        static void Main()
+        {
+            int lengthInCm = int.Parse(Console.ReadLine());
+            int widthInCm = int.Parse(Console.ReadLine());
+            int heigthInCm = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+            percent *= 0.01;
+
+            double volume = lengthInCm * widthInCm * heigthInCm;
+            double totalLitters = volume * 0.001;
+
+            double result = totalLitters - (totalLitters *percent);
+            Console.WriteLine($"{result:f3}");
+        }
+    }
+}
