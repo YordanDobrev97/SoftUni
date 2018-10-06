149a545bfd2326bad076e1b054fbee9728e8e912
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2018-10-06 09:58:14 +0300

    Adding task and resources

---
+++ Programming Basics with C#/08.While Loop/Homework/09.Moving/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace _09.Moving
+{
+    class Program
+    {
+        static void Main()
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            int maxCubMeters = width * length * height;
+            while (command != "Done")
+            {
+                int numberCartons = int.Parse(command);
+                maxCubMeters -= numberCartons;
+
+                if (maxCubMeters < 0)
+                {
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (maxCubMeters >=0)
+            {
+                Console.WriteLine($"{maxCubMeters} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs(maxCubMeters)} Cubic meters more.");
+            }
+        }
+
+    }
+}
