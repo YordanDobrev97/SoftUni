cffd18d8d61c2f1422d055ca9b451f6d8f20860b
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2018-10-15 21:50:13 +0300

    Exam Preparation(Programming Basics)

---
+++ Programming Basics with C#/14.Exams/Exam Preparation/01.PC Store/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.PC_Store
+{
+    class Program
+    {
+        static void Main()
+        {
+            double priceCPU = double.Parse(Console.ReadLine());
+            double priceVideoCard = double.Parse(Console.ReadLine());
+            double priceRam = double.Parse(Console.ReadLine());
+            int numbersRam = int.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+
+            priceCPU *= 1.57;
+            priceVideoCard *= 1.57;
+            priceRam *= 1.57;
+
+            double totalPriceRam = priceRam * numbersRam;
+            double priceCpuAfterDiscount = priceCPU - (priceCPU * discount);
+            double priceVideoCardAfterDiscount = priceVideoCard - (priceVideoCard * discount);
+
+            double totalPrice = totalPriceRam + priceCpuAfterDiscount + priceVideoCardAfterDiscount;
+
+            Console.WriteLine($"Money needed - {totalPrice:f2} leva.");
+        }
+    }
+}
---
+++ Programming Basics with C#/14.Exams/Exam Preparation/02.Shopping/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double budget = double.Parse(Console.ReadLine());
+        int numberOfChocolate = int.Parse(Console.ReadLine());
+        double quantityMilk = double.Parse(Console.ReadLine());
+
+        double priceChocolate = numberOfChocolate * 0.65;
+        double priceMilk = double.Parse($"{quantityMilk * 2.70:f2}");
+
+        double numberMandarins = Math.Floor(numberOfChocolate - (numberOfChocolate * 0.35));
+        numberMandarins *= 0.20;
+
+        double totalPrice = double.Parse($"{priceChocolate + priceMilk + numberMandarins}");
+
+        if (totalPrice <= budget)
+        {
+            Console.WriteLine($"You gotthis, {budget - totalPrice:f2} money left!");
+        }
+        else
+        {
+            Console.WriteLine($"Not enough money, you need {totalPrice - budget:f2} more!");
+        }
+    }
+}
+
