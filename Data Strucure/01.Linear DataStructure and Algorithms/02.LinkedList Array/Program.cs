6bc60b21099f31b39b62e59e7d74f1f65b2245cc
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2018-11-05 00:28:04 +0200

    Implementation of ArrayList with Linked List - DS

---
+++ Data Strucure/01.Linear DataStructure and Algorithms/02.LinkedList Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        LinkedListArray<int> linkedListArray = new LinkedListArray<int>();
+        linkedListArray.Add(1);
+        linkedListArray.Add(2);
+        linkedListArray.Add(3);
+        linkedListArray.Add(4);
+        linkedListArray.Add(5);
+        linkedListArray.Add(6);
+
+        foreach (var item in linkedListArray)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
