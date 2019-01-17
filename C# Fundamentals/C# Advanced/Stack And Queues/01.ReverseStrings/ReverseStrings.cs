4c4d69f8b943219d07f568af49a5aa121ffb02c2
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-01-17 17:08:12 +0200

    Stacks and Queues
    Lab C# Fundamentals

---
+++ C# Fundamentals/C# Advanced/Stack And Queues/01.ReverseStrings/ReverseStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseStrings
+{
+    public class ReverseStrings
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack<char> strings = new Stack<char>();
+
+            foreach (var item in input)
+            {
+                strings.Push(item);
+            }
+
+            Console.WriteLine(string.Join("", strings));
+        }
+    }
+}
---
+++ C# Fundamentals/C# Advanced/Stack And Queues/05.Supermarket/Supermarket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.Supermarket
+{
+    public class Supermarket
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+
+            Queue<string> names = new Queue<string>();
+            while (name != "End")
+            {
+                if (name == "Paid")
+                {
+                    PrintNames(names);
+                }
+                else
+                {
+                    names.Enqueue(name);
+                }
+
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+
+        private static void PrintNames(Queue<string> names)
+        {
+            while (names.Count > 0)
+            {
+                Console.WriteLine(names.Dequeue());
+            }
+        }
+    }
+}
