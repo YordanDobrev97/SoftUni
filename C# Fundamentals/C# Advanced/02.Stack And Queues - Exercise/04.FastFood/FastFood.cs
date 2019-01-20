e7797fb4995fffc74840d861c6f31f938ee90026
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-01-20 22:26:37 +0200

    CSharp Fundamentals Exercise

--- C# Fundamentals/C# Advanced/02.Stack And Queues - Exercise/04.FastFood/FastFood.cs
+++ C# Fundamentals/C# Advanced/02.Stack And Queues - Exercise/04.FastFood/FastFood.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04.FastFood
 {
@@ -6,7 +8,32 @@ namespace _04.FastFood
     {
         public static void Main()
         {
-            //TODO...
+            int quantityFood = int.Parse(Console.ReadLine());
+            int[] order = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < order.Length; i++)
+            {
+                int foodQuantity = order[i];
+                queue.Enqueue(foodQuantity);
+                quantityFood -= foodQuantity;
+            }
+
+            int max = queue.Max();
+
+            Console.WriteLine(max);
+            if (quantityFood > 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine("Orders left: 76");
+            }
         }
     }
 }
---
+++ C# Fundamentals/C# Advanced/02.Stack And Queues - Exercise/05.FashionBoutique/FashionBoutique.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    public class FashionBoutique
+    {
+        public static void Main()
+        {
+            int[] clothesInBox = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int capacityBox = int.Parse(Console.ReadLine());
+
+            Stack<int> clothes = new Stack<int>(clothesInBox);
+
+            int currentCapacity = 0;
+            int numberBox = 0;
+            while (clothes.Count > 0)
+            {
+                int value = 0;
+                while (currentCapacity < capacityBox)
+                {
+                    if (clothes.Count == 0)
+                    {
+                        numberBox++;
+                        break;
+                    }
+                    value = clothes.Pop();
+                    currentCapacity += value;
+                }
+
+                if (currentCapacity > capacityBox)
+                {
+                    numberBox++;
+                    clothes.Push(value);
+                    currentCapacity = 0;
+                }
+                else if(currentCapacity == capacityBox)
+                {
+                    numberBox++;
+                    currentCapacity = 0;
+                }
+            }
+
+            Console.WriteLine(numberBox);
+        }
+    }
+}
