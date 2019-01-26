f0e6e3ba06367d805c24a92a137bc2d3f8064a21
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-01-26 19:30:31 +0200

    CSharp Advanced Stack and Queues

--- C# Fundamentals/C# Advanced/02.Stack And Queues - Exercise/09.SimpleTextEditor/SimpleTextEditor.cs
+++ C# Fundamentals/C# Advanced/02.Stack And Queues - Exercise/09.SimpleTextEditor/SimpleTextEditor.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _09.SimpleTextEditor
 {
@@ -8,22 +10,40 @@ namespace _09.SimpleTextEditor
         {
             int numberOperations = int.Parse(Console.ReadLine());
 
+            Stack<string> operations = new Stack<string>();
+            StringBuilder text = new StringBuilder();
+
             for (int i = 0; i < numberOperations; i++)
             {
                 string[] elements = Console.ReadLine()
                     .Split(' ');
+
+                string value = string.Empty;
+                if (elements.Length == 2)
+                {
+                    value = elements[1];
+                }
 
                 string command = elements[0];
 
                 switch (command)
                 {
                     case "1":
+                        operations.Push(text.ToString());
+                        text.Append(value);
                         break;
                     case "2":
+                        operations.Push(text.ToString());
+                        int countRemove = int.Parse(value);
+                        text.Remove(text.Length - countRemove, countRemove);
                         break;
                     case "3":
+                        int index = int.Parse(value);
+                        Console.WriteLine(text[index - 1]);
                         break;
                     case "4":
+                        text.Clear();
+                        text.Append(operations.Pop());
                         break;
                 }
             }
---
+++ C# Fundamentals/C# Advanced/02.Stack And Queues - Exercise/11.KeyRevolver/KeyRevolver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.KeyRevolver
+{
+    public class KeyRevolver
+    {
+        public static void Main()
+        {
+            int priceOfBullet = int.Parse(Console.ReadLine());
+            int sizeOfBarrel = int.Parse(Console.ReadLine());
+            List<int> bulletsInput = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> locksInput = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            Stack<int> bullets = new Stack<int>(bulletsInput);
+            Queue<int> locks = new Queue<int>(locksInput);
+            int valueOfIntelligence = int.Parse(Console.ReadLine());
+
+            int shoots = 0;
+            while (bullets.Count > 0 && locks.Count > 0)
+            {
+                int currentLastBullet = bullets.Pop();
+                int currentLock = locks.Peek();
+
+                if (currentLastBullet <= currentLock)
+                {
+                    Console.WriteLine($"Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                shoots++;
+
+                if (shoots % sizeOfBarrel == 0 && bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                }
+                
+            }
+            if (locks.Count <= 0)
+            {
+                int bulletCost = shoots * priceOfBullet;
+                int earned = valueOfIntelligence - bulletCost;
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${earned}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+        }
+    }
+}
