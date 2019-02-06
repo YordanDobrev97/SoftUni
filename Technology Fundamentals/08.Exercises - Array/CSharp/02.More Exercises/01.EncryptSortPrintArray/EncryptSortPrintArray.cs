4d8ada82a9e7eedddbf915ea973cac8177dd2439
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-02-06 22:56:42 +0200

    Technology Fundamentals More Exercise Arrays

---
+++ Technology Fundamentals/08.Exercises - Array/CSharp/02.More Exercises/01.EncryptSortPrintArray/EncryptSortPrintArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.EncryptSortPrintArray
+{
+    public class EncryptSortPrintArray
+    {
+        public static void Main()
+        {
+            int numbersLines = int.Parse(Console.ReadLine());
+
+            string[] names = new string[numbersLines];
+            for (int i = 0; i < numbersLines; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+
+            int[] encryptValues = new int[numbersLines];
+            char[] vowelsAlhabets =
+            {
+                'a', 'e', 'i', 'o', 'u',
+                'A', 'E', 'I', 'O','U',
+            };
+
+            int index = 0;
+            foreach (var name in names)
+            {
+                int sum = 0;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    char symbol = name[i];
+                    int asciiValue = symbol;
+                    if (vowelsAlhabets.Contains(symbol))
+                    {
+                        sum += asciiValue * name.Length;
+                    }
+                    else
+                    {
+                        sum += asciiValue / name.Length;
+                    }
+                }
+                encryptValues[index] = sum;
+                index++;
+            }
+
+            Array.Sort(encryptValues);
+
+            foreach (var encryptValue in encryptValues)
+            {
+                Console.WriteLine(encryptValue);
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals/08.Exercises - Array/CSharp/02.More Exercises/02.PascalTriangle/PascalTriangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02.PascalTriangle
+{
+    public class PascalTriangle
+    {
+        public static void Main()
+        {
+            int trianglePascalSize = int.Parse(Console.ReadLine());
+
+            int[] lastValuesArray = null;
+            for (int i = 0; i < trianglePascalSize; i++)
+            {
+                int[] numbers = new int[i + 1];
+                numbers[0] = 1;
+
+                int currentIndexSave = 3;
+
+                if (currentIndexSave <=numbers.Length)
+                {
+                    //TODO...
+                    int lastValue = lastValuesArray[i - 2];
+
+                }
+                else if (numbers.Length == 2)
+                {
+                    numbers[1] = 1;
+                }
+
+                lastValuesArray = numbers;
+
+                foreach (var number in numbers)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
