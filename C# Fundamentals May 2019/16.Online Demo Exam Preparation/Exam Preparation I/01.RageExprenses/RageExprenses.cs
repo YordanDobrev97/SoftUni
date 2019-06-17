cfd0c4853c9e9c1b52173c983ea608f6efa4b712
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-06-17 20:23:54 +0300

    Exam Preparation for online mid exam

---
+++ C# Fundamentals May 2019/16.Online Demo Exam Preparation/Exam Preparation I/01.RageExprenses/RageExprenses.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _01.RageExprenses
+{
+    class Program
+    {
+        static void Main()
+        {
+            int gamesCount = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int countTrashHeadset = 0;
+            int countTrashMouse = 0;
+            int countTrashKeyboard = 0;
+            int countTrashDisplay = 0;
+
+            for (int game = 1; game <= gamesCount; game++)
+            {
+                if (game % 2 == 0)
+                {
+                    countTrashHeadset++;
+                }
+
+                if (game % 3 == 0)
+                {
+                    countTrashMouse++;
+                }
+
+                if (game % 2 == 0 && game % 3 == 0)
+                {
+                    countTrashKeyboard++;
+
+                    if (countTrashKeyboard % 2 == 0)
+                    {
+                        countTrashDisplay++;
+                    }
+                }
+            }
+
+            double priceOfHeadSet = headsetPrice * countTrashHeadset;
+            double priceOfMouse = mousePrice * countTrashMouse;
+            double priceOfKeyboard = keyboardPrice * countTrashKeyboard;
+            double priceOfDisplay = displayPrice * countTrashDisplay;
+
+            double totalPrice = priceOfHeadSet + priceOfMouse + priceOfKeyboard + priceOfDisplay;
+
+            Console.WriteLine($"Rage expenses: {totalPrice:f2} lv.");
+        }
+    }
+}
---
+++ C# Fundamentals May 2019/16.Online Demo Exam Preparation/Exam Preparation I/02.MemoryView/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _02.MemoryView
+{
+    class Program
+    {
+        static void Main()
+        {
+            string line = Console.ReadLine();
+
+            List<string> words = new List<string>();
+            while (line != "Visual Studio crash")
+            {
+                List<string> splitedInput = line.Split(new char[] {' '}, 
+                    StringSplitOptions.RemoveEmptyEntries)
+                        .Where(x => x != "0").ToList();
+
+                int index = (splitedInput.IndexOf("32763")) + 2;
+
+                string result = string.Empty;
+                for (int i = index; i < splitedInput.Count; i++)
+                {
+                    int value = int.Parse(splitedInput[i]);
+                    result += (char)value;
+                }
+                words.Add(result);
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
