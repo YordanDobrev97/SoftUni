26fbc45178df2e8871ad6d60da62e4e3eaece0e0
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-06-26 21:54:05 +0300

    Mid exam preparation

---
+++ C# Fundamentals May 2019/18.Exam Preparation/Mid Exam - 10 March 2019 Group 1/01.SpringVacationTrip/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _01.SpringVacationTrip
+{
+    class Program
+    {
+        static void Main()
+        {
+            int days = int.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+            int countPeople = int.Parse(Console.ReadLine());
+            double priceFuelPerKm = double.Parse(Console.ReadLine());
+            double priceFood = double.Parse(Console.ReadLine());
+            double priceHotel = double.Parse(Console.ReadLine());
+
+            double cost = 0;
+            double foodPrice = priceFood * countPeople * days;
+            double hotelPrice = priceHotel * countPeople * days;
+
+            if (countPeople > 10)
+            {
+                hotelPrice -= hotelPrice * 0.25;
+            }
+
+            double totalPrice = foodPrice + hotelPrice;
+
+            for (int day = 1; day <=days; day++)
+            {
+                double distance = double.Parse(Console.ReadLine());
+
+                double currentFuelKm = distance * priceFuelPerKm;
+                totalPrice += currentFuelKm;
+
+                if (day % 3 == 0 || day % 5 == 0)
+                {
+                    totalPrice += totalPrice * 0.40;
+                }
+
+                if (day % 7 == 0)
+                {
+                    totalPrice -= totalPrice / countPeople;
+                }
+                cost = totalPrice;
+            }
+
+            if (cost <= budget)
+            {
+                Console.WriteLine($"You have reached the destination. You have {budget - cost:f2}$ budget left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money to continue the trip. You need {cost - budget:f2}$ more.");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals May 2019/18.Exam Preparation/Mid Exam - 10 March 2019 Group 1/02.HelloFrance/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.HelloFrance
+{
+    class Program
+    {
+        static void Main()
+        {
+            var items = Console.ReadLine().Split("|").ToList();
+            var budget = double.Parse(Console.ReadLine());
+            var fullBudget = budget;
+
+            List<double> newPrice = new List<double>();
+            var maxPriceClothes = 50.00;
+            var maxPriceShoes = 35.00;
+            var maxPriceAccessories = 20.50;
+
+            foreach (var item in items)
+            {
+                var args = item.Split("->");
+                var type = args[0];
+                var price = double.Parse(args[1]);
+
+                switch (type)
+                {
+                    case "Clothes":
+                        if (price <= maxPriceClothes && budget - price > 0)
+                        {
+                            budget -= price;
+                            newPrice.Add(price);
+                        }
+                        break;
+                    case "Shoes":
+                        if (price <= maxPriceShoes && budget - price > 0)
+                        {
+                            budget -= price;
+                            newPrice.Add(price);
+                        }
+                        break;
+                    case "Accessories":
+                        if (price <= maxPriceAccessories && budget - price> 0)
+                        {
+                            budget -= price;
+                            newPrice.Add(price);
+                        }
+                        break;
+                }
+            }
+            if (newPrice.Any())
+            {
+                newPrice = newPrice.Select(x => x + (x * 0.4)).ToList();
+            }
+            
+            var sumPrice = newPrice.Sum() + budget;
+
+            double profit = Math.Abs(sumPrice - fullBudget);
+
+            if (newPrice.Count != 0)
+            {
+                foreach (var item in newPrice)
+                {
+                    Console.Write($"{item:f2} ");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine($"Profit: {profit:f2}");
+
+            if (sumPrice >= 150)
+            {
+                Console.WriteLine("Hello, France!");
+            }
+            else
+            {
+                Console.WriteLine("Time to go.");
+            }
+        }
+    }
+}
