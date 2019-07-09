67183e88c3ccf89627f2723c9cf4e634aa42c133
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-07-09 14:48:38 +0300

    Intro to Database

---
+++ C# Fundamentals May 2019/23.Database Basics/create-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Students (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	age INT NOT NULL,
+	grade DOUBLE NOT NULL
+)
---
+++ C# Fundamentals May 2019/23.Database Basics/insert-in-table.sql
@@ -0,0 +1,17 @@
+INSERT INTO students (first_name, last_name, age, grade)
+VALUES ('Guy', 'Gilbert', 15, 4.5)
+
+INSERT INTO students (first_name, last_name, age, grade)
+VALUES ('Kevin', 'Brown', 17, 5.4)
+
+INSERT INTO students (first_name, last_name, age, grade)
+VALUES ('Roberto', 'Tamburello', 19, 6)
+
+INSERT INTO students (first_name, last_name, age, grade)
+VALUES ('Linda', 'Smith', 18, 5)
+
+INSERT INTO students (first_name, last_name, age, grade)
+VALUES ('John', 'Stones', 16, 4.25)
+
+INSERT INTO students (first_name, last_name, age, grade)
+VALUES ('Nicole', 'Nelson', 17, 5.50)
