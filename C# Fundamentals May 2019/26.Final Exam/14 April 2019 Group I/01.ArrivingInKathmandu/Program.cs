905767b8d61645cc8bb46279b8dbaf868b496455
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-07-30 13:51:37 +0300

    Exam14 April Final Exam

---
+++ C# Fundamentals May 2019/26.Final Exam/14 April 2019 Group I/01.ArrivingInKathmandu/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace _01.ArrivingInKathmandu
+{
+    class Program
+    {
+        static void Main()
+        {
+            string line = Console.ReadLine();
+            var pattern = @"^([A-Za-z0-9!@#$?]+)=(\d+)<<(.+)$";
+
+            while (line != "Last note")
+            {
+                var match = Regex.Match(line, pattern);
+
+                if (match.Success)
+                {
+                    var name = match.Groups[1].Value;
+                    var length = int.Parse(match.Groups[2].Value);
+                    var hashCode = match.Groups[3].Value;
+
+                    if (length == hashCode.Length)
+                    {
+                        name = Regex.Replace(name, "[!@#$?]", "");
+                        Console.WriteLine($"Coordinates found! {name} -> {hashCode}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ C# Fundamentals May 2019/26.Final Exam/14 April 2019 Group I/02.On the WayAnnapurna/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.On_the_WayAnnapurna
+{
+    class Program
+    {
+        static void Main()
+        {
+            string line = Console.ReadLine();
+
+            Dictionary<string, List<string>> storeItems = new Dictionary<string, List<string>>();
+
+            while (line != "END")
+            {
+                string[] elements = line.Split("->");
+
+                string command = elements[0];
+                string store = elements[1];
+
+                if (command == "Add")
+                {
+                    List<string> items = elements[2].Split(",").ToList();
+                    if (!storeItems.ContainsKey(store))
+                    {
+                        storeItems.Add(store, new List<string>());
+                    }
+
+                    storeItems[store].AddRange(items);
+                }
+                else if (command == "Remove")
+                {
+                    if (storeItems.ContainsKey(store))
+                    {
+                        storeItems.Remove(store);
+                    }
+                }
+                
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine("Stores list:");
+            foreach (var item in storeItems.OrderByDescending(x => x.Value.Count).
+                ThenByDescending(x => x.Key))
+            {
+                Console.WriteLine(item.Key);
+
+                foreach (var value in item.Value)
+                {
+                    Console.WriteLine($"<<{value}>>");
+                }
+            }
+        }
+    }
+}
