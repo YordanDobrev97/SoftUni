3325387532750504102b6c384ef4fc011f10bca0
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-07-30 16:23:26 +0300

    Part II Exam 14 April

---
+++ C# Fundamentals May 2019/26.Final Exam/14 April 2019 Group II/01.IsleManRace/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.IsleManRace
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pattern = new Regex(@"^([#$%*&])(?<racer>\w+)\1=(?<length>\d+)!!(?<hash>.*)$");
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                var match = pattern.Match(line);
+
+                if (match.Success)
+                {
+                    var racer = match.Groups["racer"].Value;
+                    var length = int.Parse(match.Groups["length"].Value);
+                    var hash = match.Groups["hash"].Value;
+
+                    if (length == hash.Length)
+                    {
+                        string result = string.Empty;
+
+                        for (int i = 0; i < hash.Length; i++)
+                        {
+                            result += (char)(hash[i] + length);
+                        }
+
+                        Console.WriteLine($"Coordinates found! {racer} -> {result}");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals May 2019/26.Final Exam/14 April 2019 Group II/02. Practice Sessions/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Practice_Sessions
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, List<string>> roadRacers = new Dictionary<string, List<string>>();
+            string line = Console.ReadLine();
+
+            while (line != "END")
+            {
+                string[] elements = line.Split("->");
+
+                string command = elements[0];
+                string road = elements[1];
+
+                if (command == "Add")
+                {
+                    string racer = elements[2];
+                    if (!roadRacers.ContainsKey(road))
+                    {
+                        roadRacers.Add(road, new List<string>());
+                    }
+
+                    roadRacers[road].Add(racer);
+                }
+                else if (command == "Move")
+                {
+                    string currentRoad = elements[1];
+                    string racer = elements[2];
+                    string newRoad = elements[3];
+
+                    var racers = roadRacers[currentRoad];
+                    if (racers.Contains(racer))
+                    {
+                        racers.Remove(racer);
+
+                        roadRacers[newRoad].Add(racer);
+                    }
+                }
+                else if  (command == "Close")
+                {
+                    if (roadRacers.ContainsKey(road))
+                    {
+                        roadRacers.Remove(road);
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine("Practice sessions:");
+            foreach (var item in roadRacers.OrderByDescending(x => x.Value.Count).
+                ThenBy(x => x.Key))
+            {
+                Console.WriteLine(item.Key);
+
+                foreach (var racer in item.Value)
+                {
+                    Console.WriteLine($"++{racer}");
+                }
+            }
+        }
+    }
+}
