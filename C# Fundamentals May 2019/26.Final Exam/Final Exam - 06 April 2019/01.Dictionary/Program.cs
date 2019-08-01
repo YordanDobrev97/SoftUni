62ce61fafecde1a91db295801befb8d9b734791e
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-08-01 11:39:55 +0300

    Final Exam 6 April 2019

---
+++ C# Fundamentals May 2019/26.Final Exam/Final Exam - 06 April 2019/01.Dictionary/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Dict
+{
+    class Program
+    {
+        static void Main()
+        {
+            string line = Console.ReadLine();
+
+            var words = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                if (line == "End")
+                {
+                    break;
+                }
+                else if (line == "List")
+                {
+                    var wordsKey = words.Keys.ToList();
+                    wordsKey.Sort();
+
+                    Console.WriteLine(string.Join(" ", wordsKey));
+                    break;
+                }
+                string[] elements = line.Split(" | ");
+
+                foreach (var element in elements)
+                {
+                    string word = element;
+                    string defination = string.Empty;
+
+                    if (element.Contains(":"))
+                    {
+                        word = element.Split(": ")[0].Trim();
+                        defination = element.Split(": ")[1].Trim();
+
+                        if (!words.ContainsKey(word))
+                        {
+                            words.Add(word, new List<string>());
+                        }
+
+                        if (!words[word].Contains(defination))
+                        {
+                            words[word].Add(defination);
+                        }
+                    }
+                    else
+                    {
+                        foreach (var item in elements)
+                        {
+                            if (!words.ContainsKey(item))
+                            {
+                                continue;
+                            }
+                            Console.WriteLine($"{item}");
+                            foreach (var definationWord in words[item].
+                                OrderByDescending(x => x.Length))
+                            {
+                                Console.WriteLine($" -{definationWord}");
+                            }
+                        }
+                        break;
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ C# Fundamentals May 2019/26.Final Exam/Final Exam - 06 April 2019/02.Deciphering/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace _02.Deciphering
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstString = Console.ReadLine();
+            string[] secondString = Console.ReadLine().Split();
+
+            StringBuilder newString = new StringBuilder();
+
+            for (int i = 0; i < firstString.Length; i++)
+            {
+                newString.Append((char)(firstString[i] - 3));
+            }
+
+            string foundString = secondString[0];
+            string replacment = secondString[1];
+
+            newString = newString.Replace(foundString, replacment);
+            Console.WriteLine(newString);
+        }
+    }
+}
