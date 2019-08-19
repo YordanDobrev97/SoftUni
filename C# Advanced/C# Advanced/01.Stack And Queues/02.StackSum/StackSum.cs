1590ca6d5c2217b8532dc7f543b96eac004e15b5
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-08-19 11:22:35 +0300

    CSharp Advanced Stacks And Queues Lab

---
+++ C# Advanced/C# Advanced/01.Stack And Queues/02.StackSum/StackSum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.StackSum
+{
+    public class StackSum
+    {
+        public static void Main()
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> numbers = new Stack<int>();
+
+            foreach (var item in inputNumbers)
+            {
+                numbers.Push(item);
+            }
+
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "end")
+            {
+                string[] values = command.Split(' ');
+
+                if (values[0].ToLower() == "add")
+                {
+                    int firstNumber = int.Parse(values[1]);
+                    int secondNumber = int.Parse(values[2]);
+
+                    numbers.Push(firstNumber);
+                    numbers.Push(secondNumber);
+                }
+                else if(values[0].ToLower() == "remove")
+                {
+                    int countRemove = int.Parse(values[1]);
+
+                    if (countRemove < numbers.Count())
+                    {
+                        for (int i = 0; i < countRemove; i++)
+                        {
+                            numbers.Pop();
+                        }
+                    }
+                }
+
+                command = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine($"Sum: {numbers.Sum()}");
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced/01.Stack And Queues/03.SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SimpleCalculator
+{
+    public class SimpleCalculator
+    {
+        public static void Main()
+        {
+            string[] expression = Console.ReadLine().Split(" ");
+            Stack<string> stack = new Stack<string>();
+
+            int sum = 0;
+            foreach (var item in expression)
+            {
+                stack.Push(item);
+            }
+
+            stack.Reverse();
+            while (stack.Count > 0)
+            {
+
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
