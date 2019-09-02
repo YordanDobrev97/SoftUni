d497c3f2ca55ae48b9e326ef0e56bc8595bfc693
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-09-02 23:22:34 +0300

    Stream Files and Directories Lab
    Odd Lines

---
+++ C# Advanced/C# Advanced/07.Streams, Files and Directories/StreamsFilesDirectories/01.OddLines/OddLines.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    public class OddLines
+    {
+        public static void Main()
+        {
+            using (var reader= new StreamReader(@"Resources\01.Odd Lines\Input.txt"))
+            {
+                string line;
+                int index = 0;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (index % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    index++;
+                }
+            }
+        }
+    }
+}
