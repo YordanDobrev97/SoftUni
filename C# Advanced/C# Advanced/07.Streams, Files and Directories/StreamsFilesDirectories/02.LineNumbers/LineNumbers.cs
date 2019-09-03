cb725e80875acbe792994e263c953840bb3044cf
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-09-03 22:56:46 +0300

    Streams Line Numbers

---
+++ C# Advanced/C# Advanced/07.Streams, Files and Directories/StreamsFilesDirectories/02.LineNumbers/LineNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    public class LineNumbers
+    {
+        public static void Main()
+        {
+            using (var reader = new StreamReader("Input.txt"))
+            {
+                var writer = new StreamWriter("Output.txt");
+                int counter = 1;
+
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    writer.WriteLine($"{counter}.{line}");
+                    line = reader.ReadLine();
+                    counter++;
+                }
+
+                writer.Close();
+            }
+        }
+    }
+}
