bcb2090769ab1a841b4818b8cdb19a88ac72449a
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-09-18 14:08:57 +0300

    Stacks Addiing New Task (Advanced)

---
+++ C# Advanced/C# Advanced/02.Stack And Queues - Exercise/06.SongsQueue/SongsQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.SongsQueue
+{
+    class SongsQueue
+    {
+        static void Main()
+        {
+            string[] songInput = Console.ReadLine()
+                .Split(", ");
+
+            Queue<string> songs = new Queue<string>(songInput);
+
+            while (songs.Count > 0)
+            {
+                string input = Console.ReadLine();
+
+                if (input.Contains("Add"))
+                {
+                    string song = input.Substring(4);
+
+                    if (!songs.Contains(song))
+                    {
+                        songs.Enqueue(song);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                }
+                else if (input.Contains("Play"))
+                {
+                    songs.Dequeue();
+                }
+                else if (input.Contains("Show"))
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
