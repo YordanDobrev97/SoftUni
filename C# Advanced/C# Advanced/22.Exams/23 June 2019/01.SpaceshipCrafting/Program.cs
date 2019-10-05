dbe96399dd5fb60d2e894549316e0d517b086be1
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-10-05 21:16:03 +0300

    Exams Advanced

---
+++ C# Advanced/C# Advanced/22.Exams/23 June 2019/01.SpaceshipCrafting/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SpaceshipCrafting
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] chemicalLiquidsInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] physicalItemsInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> physicalItems = new Stack<int>(physicalItemsInput);
+            Queue<int> chemicalLiquids = new Queue<int>(chemicalLiquidsInput);
+
+            Stack<string[]> materials = new Stack<string[]>();
+            materials.Push(new string[2] { "Aluminium", "0" });
+            materials.Push(new string[2] { "Carbon fiber", "0" });
+            materials.Push(new string[2] { "Glass", "0" });
+            materials.Push(new string[2] { "Lithium", "0" });
+
+            int[] validValues = {25,50,75,100};
+
+            while (physicalItems.Count > 0 && chemicalLiquids.Count > 0)
+            {
+                int firstLiquid = chemicalLiquids.Dequeue();
+                int lastPhysicalItem = physicalItems.Pop();
+
+                int sum = firstLiquid + lastPhysicalItem;
+
+                if (!validValues.Contains(sum))
+                {
+                    lastPhysicalItem += 3;
+                    physicalItems.Push(lastPhysicalItem);
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced/22.Exams/23 June 2019/02.SpaceStationRecruitment/SpaceStation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.SpaceStationRecruitment
+{
+    public class SpaceStation
+    {
+        public SpaceStation(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Astronauts = new List<Astronaut>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => this.Astronauts.Count;
+
+        public List<Astronaut> Astronauts { get; set; }
+
+        public void Add(Astronaut astronaut)
+        {
+            if (this.Astronauts.Count <= this.Capacity)
+            {
+                this.Astronauts.Add(astronaut);
+            }
+        }
+
+        public string Report()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Astronauts working at Space Station {this.Name}:");
+
+            foreach (var item in this.Astronauts)
+            {
+                result.AppendLine($"Astronaut: {item.Name}, {item.Age} ({item.Country})");
+            }
+
+            return result.ToString();
+        }
+
+        public bool Remove(string name)
+        {
+            var currentAstronaut = this.Astronauts.FirstOrDefault(x => x.Name == name);
+
+            if (currentAstronaut != null)
+            {
+                this.Astronauts.Remove(currentAstronaut);
+                return true;
+            }
+            return false;
+        }
+
+        public Astronaut GetOldestAstronaut()
+        {
+            Astronaut oldestAstronaut = null;
+
+            int oldestAge = 0;
+            foreach (var item in this.Astronauts)
+            {
+                if (oldestAge < item.Age)
+                {
+                    oldestAstronaut = item;
+                    oldestAge = item.Age;
+                }
+            }
+
+            return oldestAstronaut;
+        }
+
+        public Astronaut GetAstronaut(string name)
+        {
+            return this.Astronauts.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
