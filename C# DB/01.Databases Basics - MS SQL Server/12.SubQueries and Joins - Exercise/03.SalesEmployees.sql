37c5e37ab452535de5116a9953ec75e34365b67e
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-10-06 21:44:01 +0300

    Database SubQueries and Joins Exercise

---
+++ C# DB/01.Databases Basics - MS SQL Server/12.SubQueries and Joins - Exercise/03.SalesEmployees.sql
@@ -0,0 +1,8 @@
+SELECT EmployeeID, 
+	   FirstName, 
+	   LastName, 
+	   d.Name AS [DepartmentName] 
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE d.Name = 'Sales'
+ORDER BY e.EmployeeID
---
+++ C# DB/01.Databases Basics - MS SQL Server/12.SubQueries and Joins - Exercise/04.EmployeeDepartments.sql
@@ -0,0 +1,5 @@
+SELECT TOP 5 EmployeeID, FirstName, Salary, d.[Name] 
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE e.Salary > 15000
+ORDER BY d.DepartmentID
