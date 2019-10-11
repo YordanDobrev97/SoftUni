de7b42b2ddde6689482fe47e180ebe51f6294b78
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-10-11 21:30:08 +0300

    Custom Stack - Workshop

---
+++ C# Advanced/C# Advanced/12.Workshop/Workshop/CustomStack/CustomStack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class CustomStack<T>
+    {
+        private const int DefaultValue = 4;
+        private T[] array;
+
+        public CustomStack()
+        {
+            this.array = new T[DefaultValue];
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(T element)
+        {
+            if (this.Count >= this.array.Length)
+            {
+                Resize();
+            }
+
+            this.array[this.Count] = element;
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new ArgumentOutOfRangeException("Stack empty");
+            }
+
+            T lastElement = this.array[this.Count - 1];
+            this.Count--;
+            return lastElement;
+        }
+
+        public T Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new ArgumentOutOfRangeException("Stack empty");
+            }
+
+            T lastElement = this.array[this.Count - 1];
+            return lastElement;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                action(this.array[i]);
+            }
+        }
+
+        private void Resize()
+        {
+            T[] newArray = new T[this.array.Length * 2];
+            Array.Copy(this.array, newArray, this.array.Length);
+            this.array = newArray;
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced/12.Workshop/Workshop/CustomStack/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CustomStack<int> numberStack = new CustomStack<int>();
+            numberStack.Push(1);
+            numberStack.Push(2);
+            numberStack.Push(3);
+            numberStack.Push(4);
+            numberStack.Push(5);
+
+            numberStack.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
