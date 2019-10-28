eefe036ba1e009cd471e2b91061245cd30eef1f8
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-10-28 15:24:58 +0200

    Exams Advanced

---
+++ C# Advanced/C# Advanced/22.Exams/Retake Exam - 16 April 2019/01.TrojanInvasion/StartUp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        int wavesCount = int.Parse(Console.ReadLine());
+        List<int> spartanDefenseInput = Console.ReadLine()
+            .Split().Select(int.Parse).ToList();
+
+        Queue<int> defenseOfSpartans = new Queue<int>(spartanDefenseInput);
+        Stack<int> trojansAlive = new Stack<int>();
+
+        bool isWinSpartans = true;
+        for (int i = 1; i <= wavesCount; i++)
+        {
+            if (defenseOfSpartans.Count == 0)
+            {
+                isWinSpartans = false;
+                break;
+            }
+
+            List<int> currentWaveInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            if (i % 3 == 0)
+            {
+                int newPlate = int.Parse(Console.ReadLine());
+                defenseOfSpartans.Enqueue(newPlate);
+            }
+
+            Stack<int> trojans = new Stack<int>(currentWaveInput);
+            trojansAlive = trojans;
+
+            while (trojans.Count > 0 && defenseOfSpartans.Count > 0)
+            {
+                int currentSpartan = defenseOfSpartans.Peek();
+                int lastTrojan = trojans.Pop();
+
+                if (lastTrojan > currentSpartan)
+                {
+
+                }
+            }
+        }
+
+        if (isWinSpartans)
+        {
+            Console.WriteLine("The Spartans successfully repulsed the Trojan attack.");
+            Console.WriteLine($"Plates left: {string.Join(", ", defenseOfSpartans)}");
+        }
+        else
+        {
+            Console.WriteLine("The Trojans successfully destroyed the Spartan defense.");
+            Console.WriteLine($"Warriors left: {string.Join(", ", trojansAlive)}");
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced/22.Exams/Retake Exam - 16 April 2019/02.HelensAbduction/StartUp.cs
@@ -0,0 +1,238 @@
+﻿using System;
+
+public class StartUp
+{
+    static int parisRow;
+    static int parisCol;
+    static int energy;
+    static bool isWin = false;
+
+    public static void Main()
+    {
+        energy = int.Parse(Console.ReadLine());
+        int rows = int.Parse(Console.ReadLine());
+        char[,] matrix = new char[rows, rows];
+
+        IntilizateMatrix(matrix);
+
+        bool isDead = false;
+        while (true)
+        {
+            string[] directionParams = Console.ReadLine().Split();
+            string direction = directionParams[0];
+            int row = int.Parse(directionParams[1]);
+            int column = int.Parse(directionParams[2]);
+
+            MoveSpartan(matrix, row, column);
+            energy -= 1;
+            switch (direction)
+            {
+                case "up":
+                    if (!MoveUp(matrix))
+                    {
+                        isDead = true;
+                        break;
+                    }
+                    break;
+                case "down":
+                    if (!MoveDown(matrix))
+                    {
+                        isDead = true;
+                        break;
+                    }
+                    break;
+                case "left":
+                    if (!MoveLeft(matrix))
+                    {
+                        isDead = true;
+                        break;
+                    }
+                    break;
+                case "right":
+                    if (!MoveRight(matrix))
+                    {
+                        isDead = true;
+                        break;
+                    }
+                    break;
+            }
+
+            if (isDead)
+            {
+                matrix[parisRow, parisCol] = 'X';
+                break;
+            }
+            else if (isWin)
+            {
+                matrix[parisRow, parisCol] = '-';
+                break;
+            }
+        }
+
+        if (isDead)
+        {
+            Console.WriteLine($"Paris died at {parisRow};{parisCol}.");
+        }
+        else
+        {
+            Console.WriteLine($"Paris has successfully abducted Helen! Energy left: {energy}");
+        }
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                Console.Write($"{matrix[row,col]}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private static bool MoveRight(char[,] matrix)
+    {
+        bool isMoveSuccess = true;
+        if (parisCol + 1 <= matrix.GetLength(1) - 1)
+        {
+            matrix[parisRow, parisCol] = '-';
+            parisCol++;
+            if (matrix[parisRow, parisCol] == 'S')
+            {
+                energy -= 2;
+            }
+            else if (matrix[parisRow, parisCol] == 'H')
+            {
+                isWin = true;
+                return isWin;
+            }
+
+            if (energy == 0)
+            {
+                isMoveSuccess = false;
+            }
+            else
+            {
+                matrix[parisRow, parisCol] = 'P';
+            }
+        }
+        return isMoveSuccess;
+    }
+
+    private static bool MoveLeft(char[,] matrix)
+    {
+        bool isMoveSuccess = true;
+        if (parisCol - 1 >= 0)
+        {
+            matrix[parisRow, parisCol] = '-';
+            parisCol--;
+            if (matrix[parisRow, parisCol] == 'S')
+            {
+                energy -= 2;
+            }
+            else if (matrix[parisRow, parisCol] == 'H')
+            {
+                isWin = true;
+                return isWin;
+            }
+
+            if (energy == 0)
+            {
+                isMoveSuccess = false;
+            }
+            else
+            {
+                matrix[parisRow, parisCol] = 'P';
+            }
+        }
+        return isMoveSuccess;
+    }
+
+    private static bool MoveDown(char[,] matrix)
+    {
+        bool isMoveSuccess = true;
+        if (parisRow + 1 <= matrix.GetLength(0) - 1)
+        {
+            matrix[parisRow, parisCol] = '-';
+            parisRow++;
+            if (matrix[parisRow, parisCol] == 'S')
+            {
+                energy -= 2;
+            }
+            else if (matrix[parisRow, parisCol] == 'H')
+            {
+                isWin = true;
+                return isWin;
+            }
+
+            if (energy == 0)
+            {
+                isMoveSuccess = false;
+            }
+            else
+            {
+                matrix[parisRow, parisCol] = 'P';
+            }
+        }
+        return isMoveSuccess;
+    }
+
+    private static bool MoveUp(char[,] matrix)
+    {
+        bool isSuccessMove = true;
+        if (parisRow - 1 >= 0)
+        {
+            matrix[parisRow, parisCol] = '-';
+            parisRow--;
+            if (matrix[parisRow, parisCol] == 'S')
+            {
+                energy -= 2;
+            }
+            else if (matrix[parisRow, parisCol] == 'H')
+            {
+                isWin = true;
+                return isWin;
+            }
+
+            if (energy == 0)
+            {
+                isSuccessMove = false;
+            }
+            else
+            {
+                matrix[parisRow, parisCol] = 'P';
+            }
+        }
+
+        return isSuccessMove;
+    }
+
+    private static void MoveSpartan(char[,] matrix, int row, int col)
+    {
+        if (IsRange(matrix, row, col))
+        {
+            matrix[row, col] = 'S';
+        }
+    }
+
+    private static bool IsRange(char[,] matrix, int row, int col)
+    {
+        return row >= 0 && row <= matrix.GetLength(0) - 1 && col >=0 && col <= matrix.GetLength(1);
+    }
+
+    private static void IntilizateMatrix(char[,] matrix)
+    {
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            string line = Console.ReadLine();
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrix[row, col] = line[col];
+
+                if (matrix[row,col] == 'P')
+                {
+                    parisRow = row;
+                    parisCol = col;
+                }
+            }
+        }
+    }
+}
