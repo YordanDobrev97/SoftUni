71277989ca4ccb3ea5876af899b68344eed8a116
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-11-25 20:13:50 +0200

    Exam 16 July OPP

---
+++ C# Advanced/C# OPP/Exams/Exam - 16 July 2017/Exam - 16 July 2017/HammerHarvester.cs
@@ -0,0 +1,9 @@
+﻿
+public class HammerHarvester : Harvester
+{
+    public HammerHarvester(string id, double oreOutput, double energyRequirement) 
+        : base(id, oreOutput *= 3, energyRequirement * 2)
+    {
+    }
+}
+
---
+++ C# Advanced/C# OPP/Exams/Exam - 16 July 2017/Exam - 16 July 2017/PressureProvider.cs
@@ -0,0 +1,9 @@
+﻿
+public class PressureProvider : Provider
+{
+    public PressureProvider(string id, double energyOutput) 
+        : base(id, energyOutput * 1.5)
+    {
+    }
+}
+
