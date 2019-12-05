38b75cad3d7ba3d238d6bd5d45cda7a003ed4b31
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-12-05 11:38:31 +0200

    Desing Patterns resources

---
+++ C# Advanced/C# OPP/22.Desing Patterns/Demos/AdapterPattern/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+//2
+namespace DemosPatterns
+{
+    public interface IDevice
+    {
+        public int Charger();
+    }
+
+    public class BgDevice : IDevice
+    {
+        public int Charger()
+        {
+            return 2;
+        }
+    }
+
+    public class EnglandDevice : IDevice
+    {
+        public int Charger()
+        {
+            return 3;
+        }
+    }
+
+    public class BgLaptop : BgDevice
+    {
+    }
+
+    public class BgTelephone : BgDevice
+    {
+    }
+
+    public class EnglandLaptop : EnglandDevice
+    {
+    }
+
+    public class EnglandTelephone : EnglandDevice
+    {
+    }
+
+    public interface IAdapter
+    {
+        public int CountHoles { get; }
+    }
+
+    class Adapter : IAdapter
+    {
+        private IDevice device;
+
+        public Adapter(IDevice device)
+        {
+            this.device = device;
+        }
+
+        public int CountHoles => this.device.Charger();
+    }
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Adapter adapter = new Adapter(new BgLaptop());
+            Console.WriteLine(adapter.CountHoles);
+
+            adapter = new Adapter(new EnglandLaptop());
+            Console.WriteLine(adapter.CountHoles);
+        }
+    }
+}
---
+++ C# Advanced/C# OPP/22.Desing Patterns/Demos/BridgePattern/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace BridgePattern
+{
+    public interface ICoder
+    {
+        public string Text();
+    }
+
+    public class GeneratorCoder : ICoder
+    {
+        private ICoder coder;
+
+        public GeneratorCoder(ICoder coder)
+        {
+            this.coder = coder;
+        }
+
+        public string Text()
+        {
+            return coder.Text();
+        }
+    }
+
+    public class CSharpCode : ICoder
+    {
+        public string Text()
+        {
+            return "Console.WriteLine(\"Hello, C#!\");";
+        }
+    }
+
+    public class JavaScriptCode : ICoder
+    {
+        public string Text()
+        {
+            return "console.log(\"Hello, JavaScript!\");";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            GeneratorCoder cSharpCode = new GeneratorCoder(new CSharpCode());
+            GeneratorCoder jsCode = new GeneratorCoder(new JavaScriptCode());
+            
+            Console.WriteLine($"C# Hello World code: ");
+
+            Console.WriteLine();
+            Console.WriteLine(cSharpCode.Text());
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("JavaScript Hello World code:");
+
+            Console.WriteLine();
+            Console.WriteLine(jsCode.Text());
+        }
+    }
+}
