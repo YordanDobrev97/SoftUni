e66b496005f32f0ecc07b1f1d8ab53b3c8a51f68
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2019-12-21 20:15:15 +0200

    Exam Prepration I OPP

---
+++ C# Advanced/C# OPP/24.Exam Preparation I/01. Structure_Skeleton - Copy/PlayersAndMonsters/Core/ManagerController.cs
@@ -0,0 +1,104 @@
+﻿namespace PlayersAndMonsters.Core
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Contracts;
+    using PlayersAndMonsters.Models.BattleFields;
+    using PlayersAndMonsters.Models.BattleFields.Contracts;
+    using PlayersAndMonsters.Models.Cards;
+    using PlayersAndMonsters.Models.Cards.Contracts;
+    using PlayersAndMonsters.Models.Players;
+    using PlayersAndMonsters.Models.Players.Contracts;
+    using PlayersAndMonsters.Repositories;
+    using PlayersAndMonsters.Repositories.Contracts;
+
+    public class ManagerController : IManagerController
+    {
+        private IPlayerRepository playerRepository;
+        private ICardRepository cardRepository;
+        private IBattleField battleField;
+
+        public ManagerController()
+        {
+            this.playerRepository = new PlayerRepository();
+            this.cardRepository = new CardRepository();
+            this.battleField = new BattleField();
+        }
+
+        public string AddPlayer(string type, string username)
+        {
+            IPlayer player = null; 
+            if (type == "Beginner")
+            {
+                player = new Beginner(new CardRepository(), username);
+            }
+            else if (type == "Advanced")
+            {
+                player = new Advanced(new CardRepository(), username);
+            }
+
+            if (this.playerRepository.Players.Any(x => x.Username == player.Username))
+            {
+                return $"Player {username} already exists!";
+            }
+
+            this.playerRepository.Add(player);
+
+            return $"Successfully added player of type {type} with username: {username}";
+        }
+
+        public string AddCard(string type, string name)
+        {
+            ICard card = null;
+            if (type == "Magic")
+            {
+                card = new MagicCard(name);
+            }
+            else if (type == "Trap")
+            {
+                card = new TrapCard(name);
+            }
+
+            this.cardRepository.Add(card);
+
+            return $"Successfully added card of type {type}Card with name: {name}";
+        }
+
+        public string AddPlayerCard(string username, string cardName)
+        {
+            var card = this.cardRepository.Cards.FirstOrDefault(x => x.Name == cardName);
+            var player = this.playerRepository.Players.FirstOrDefault(x => x.Username == username);
+
+            player.CardRepository.Add(card);
+            return $"Successfully added card: {cardName} to user: {username}";
+        }
+
+        public string Fight(string attackUser, string enemyUser)
+        {
+            var attackPlayer = this.playerRepository.Players.FirstOrDefault(x => x.Username == attackUser);
+            var enemyPlayer = this.playerRepository.Players.FirstOrDefault(x => x.Username == enemyUser);
+
+            this.battleField.Fight(attackPlayer, enemyPlayer);
+            return $"Attack user health {attackPlayer.Health} - Enemy user health {enemyPlayer.Health}";
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var username in this.playerRepository.Players)
+            {
+                sb.AppendLine($"Username: {username.Username} - Health: {username.Health} – Cards {username.CardRepository.Count}");
+
+                foreach (var card in username.CardRepository.Cards)
+                {
+                    sb.AppendLine($"Card: {card.Name} - Damage: {card.DamagePoints}");
+                }
+                sb.AppendLine("###");
+            }
+
+            return sb.ToString();
+         }
+    }
+}
