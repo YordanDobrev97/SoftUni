159997c5fb3a2da4aef69daf0a8e094f0bb320d0
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2020-01-07 20:29:34 +0200

    Refactoring 3th task from array JS Advanced

--- JS Advanced/JS Advanced/04.Exercise - Arrays/03.removeAddElements.js
+++ JS Advanced/JS Advanced/04.Exercise - Arrays/03.removeAddElements.js
@@ -1,28 +1,32 @@
 function modificationNumber(array) {
-    let initialNumber = 1;
+    function modification(array) {
+      let initialNumber = 1;
 
-    let commands = {
-      'add': function add(collection) {
-          collection.push(initialNumber);
-      },
-      'remove': function removeLast(collection) {
-            collection.pop();
+      let commands = {
+        'add': function add(collection) {
+            collection.push(initialNumber);
+        },
+        'remove': function removeLast(collection) {
+              collection.pop();
+        }
+      };
+
+      let newArray = [];
+      for (let command of array) {
+          let operationCommand = commands[command];
+          operationCommand(newArray);
+          initialNumber++;
+      }
+
+      if (newArray.length === 0) {
+          console.log('Empty');
+      } else {
+          console.log(newArray.join('\n'));
       }
     };
-
-    let newArray = [];
-    for (let command of array) {
-        let operationCommand = commands[command];
-        operationCommand(newArray);
-        initialNumber++;
-    }
-
-    if (newArray.length === 0) {
-        console.log('Empty');
-    } else {
-        console.log(newArray.join('\n'));
-    }
+    modification(array);
 }
+
 modificationNumber([
     'add', 'add', 'remove', 'add', 'add'
 ]);
