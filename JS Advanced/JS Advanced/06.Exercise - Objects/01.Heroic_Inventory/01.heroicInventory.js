bf35befd66c855f5c28550415e84d89899f3dec2
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2020-01-21 19:45:38 +0200

    fix bug 100/100 Done

--- JS Advanced/JS Advanced/06.Exercise - Objects/01.Heroic_Inventory/01.heroicInventory.js
+++ JS Advanced/JS Advanced/06.Exercise - Objects/01.Heroic_Inventory/01.heroicInventory.js
@@ -5,20 +5,21 @@ function solve(input) {
         let items = key.split(" / ");
         let heroName = items[0];
         let heroLevel = Number(items[1]);
-        let itemsHero = items[2].split(", ");
+        let itemsHero = [];
+        if (items.length === 3) {
+            itemsHero = items[2].split(", ");
+        }
+        
         let resultObj = {
             name: heroName,
             level: heroLevel,
             items: itemsHero
         };
+
         result.push(resultObj);
     }
 
     let output = JSON.stringify(result);
     console.log(output);
 }
-solve([
-  "Isacc / 25 / Apple, GravityGun",
-  "Derek / 12 / BarrelVest, DestructionSword",
-  "Hes / 1 / Desolator, Sentinel, Antara"
-]);
+solve(['Jake / 1000']);
