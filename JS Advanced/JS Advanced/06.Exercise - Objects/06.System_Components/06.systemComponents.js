d397e5aad6df3b6f2735acfc49b39ec7e272848d
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2020-01-22 15:40:36 +0200

    fix bug 100/100 Done - SystemComponents

--- JS Advanced/JS Advanced/06.Exercise - Objects/06.System_Components/06.systemComponents.js
+++ JS Advanced/JS Advanced/06.Exercise - Objects/06.System_Components/06.systemComponents.js
@@ -1,35 +1,58 @@
 function solve(input) {
-    const object = {};
-   
-    for(let item of input) {
-        const parts = item.split(' | ');
-        const systemName = parts[0];
-        const componentName = parts[1];
-        const subcomponentName = parts[2];
+  const system = {};
 
-        if (!object.hasOwnProperty(systemName)) {
-            object[systemName] = {};
-        }
+  for (let item of input) {
+    const parts = item.split(" | ");
+    const systemName = parts[0];
+    const componentName = parts[1];
+    const subComponentName = parts[2];
 
-        if (!object[systemName].hasOwnProperty(componentName)) {
-            object[systemName][componentName] = [];
-        }
-        object[systemName][componentName].push(subcomponentName);
+    if (!system.hasOwnProperty(systemName)) {
+      system[systemName] = {};
     }
 
-    
+    if (!system[systemName].hasOwnProperty(componentName)) {
+      system[systemName][componentName] = [];
+    }
+    system[systemName][componentName].push(subComponentName);
+  }
+
+  const sortedKeys = Object.keys(system).sort((a, b) => {
+    if (Object.keys(system[b]).length === Object.keys(system[a]).length) {
+      return a.localeCompare(b);
+    }
+    return Object.keys(system[b]).length - Object.keys(system[a]).length;
+  });
+
+  for (let item of sortedKeys) {
+    console.log(item);
+    const components = Object.keys(system[item]).sort((a, b) => {
+      return (
+        Object.keys(system[item][b]).length -
+        Object.keys(system[item][a]).length
+      );
+    });
+    for (let component of components) {
+      console.log(`|||${component}`);
+      const subComponentName = system[item][component];
+      subComponentName.forEach(el => {
+        console.log(`||||||${el}`);
+      });
+    }
+  }
 }
-solve(['SULS | Main Site | Home Page',
-'SULS | Main Site | Login Page',
-'SULS | Main Site | Register Page',
-'SULS | Judge Site | Login Page',
-'SULS | Judge Site | Submittion Page',
-'Lambda | CoreA | A23',
-'SULS | Digital Site | Login Page',
-'Lambda | CoreB | B24',
-'Lambda | CoreA | A24',
-'Lambda | CoreA | A25',
-'Lambda | CoreC | C4',
-'Indice | Session | Default Storage',
-'Indice | Session | Default Security']
-);
+solve([
+  "SULS | Main Site | Home Page",
+  "SULS | Main Site | Login Page",
+  "SULS | Main Site | Register Page",
+  "SULS | Judge Site | Login Page",
+  "SULS | Judge Site | Submittion Page",
+  "Lambda | CoreA | A23",
+  "SULS | Digital Site | Login Page",
+  "Lambda | CoreB | B24",
+  "Lambda | CoreA | A24",
+  "Lambda | CoreA | A25",
+  "Lambda | CoreC | C4",
+  "Indice | Session | Default Storage",
+  "Indice | Session | Default Security"
+]);
