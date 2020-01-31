152cde89cd6d21a2662877ed4271342d2bb8120c
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2020-01-31 21:54:45 +0200

    uniqueSequence from Objects

--- JS Advanced/JS Advanced/06.Exercise - Objects/08.Unique_Sequences/08.uniqueSequence.js
+++ JS Advanced/JS Advanced/06.Exercise - Objects/08.Unique_Sequences/08.uniqueSequence.js
@@ -1,30 +1,32 @@
 function solve(input) {
-   const uniqueSums = {};
-   const uniqueArrays = [];
+  const uniqueSums = {};
+  const uniqueArrays = [];
 
-   let index = 0;
-   for(let item of input) {
-     let array = JSON.parse(item);
-     let sum = array.reduce((acc, value) => acc + value,0);
+  let index = 0;
+  for (let item of input) {
+    let array = JSON.parse(item);
+    let sum = array.reduce((acc, value) => acc + value, 0);
 
-     if (!uniqueSums.hasOwnProperty(sum)) {
-       uniqueSums[sum] = {index};
-       uniqueArrays.push(array);
-     }
-   }
+    if (!uniqueSums.hasOwnProperty(sum)) {
+      uniqueSums[sum] = { index };
+      uniqueArrays.push(array);
+    }
+  }
 
-   uniqueArrays.sort((a, b) => {
-      return a.length - b.length;
-   })
+  uniqueArrays.sort((a, b) => {
+    return a.length - b.length;
+  });
 
-   for(let array of uniqueArrays) {
-     let sorted = array.sort((a,b) => {
-        return b - a;
-     });
-     console.log(`[${sorted.join(', ')}]`);
-   }
+  for (let array of uniqueArrays) {
+    let sorted = array.sort((a, b) => {
+      return b - a;
+    });
+    console.log(`[${sorted.join(", ")}]`);
+  }
 }
 
-solve(["[-3, -2, -1, 0, 1, 2, 3, 4]", "[10, 1, -17, 0, 2, 13]",
-"[4, -3, 3, -2, 2, -1, 1, 0]"]
-);
+solve([
+  "[1, 0, 0, -1]",
+  "[10, 1, -17, 0, 2, 13]",
+  "[4, -3, 3, -2, 2, -1, 1, 0]"
+]);
