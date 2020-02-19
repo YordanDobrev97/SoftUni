672fba77a1b793963c6f8c4e451aeade5da444f1
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-02-19 19:33:13 +0200

    Exam 10 December 221/300

--- JS Advanced/JS Advanced/23.Exams/JS Advanced Retake Exam - 10 December 2019/01. Christmas Gifts Delivery_Resourses/app.js
+++ JS Advanced/JS Advanced/23.Exams/JS Advanced Retake Exam - 10 December 2019/01. Christmas Gifts Delivery_Resourses/app.js
@@ -1,44 +1,70 @@
 function solution() {
-  const button = document.querySelector('.card div button');
-  const ul = document.querySelector('.card ul');
+  const addButton = document.querySelector('.container section.card div button');
+  const ul = document.querySelector('ul');
 
-  button.addEventListener('click', () => {
-    const gifName = document.querySelector('.card div input');
-    const li = document.createElement('li');
-    li.setAttribute('class', 'gift');
-    li.textContent = gifName.value;
+  addButton.addEventListener('click', () => {
+    const giftName = document.querySelector('.container > section.card > div > input');
+    const li = createHTMLElement('li', giftName.value, 'gift');
+    const sendBtn = createHTMLElement('button', 'Send', null, {id: 'sendButton'});
+    const discardBtn = createHTMLElement('button', 'Discard', null, {id: 'discardButton'})
+    
+    sendBtn.addEventListener('click', sendButton);
+    discardBtn.addEventListener('click', discardButton);
 
-    const sendButton = document.createElement('button');
-    sendButton.setAttribute('id', 'sendButton');
-    sendButton.textContent = 'Send';
-
-    const discardButton = document.createElement('button');
-    discardButton.setAttribute('id', 'discard');
-    discardButton.textContent = 'Discard';
-
-    li.appendChild(sendButton);
-    li.appendChild(discardButton);
-
+    li.appendChild(sendBtn);
+    li.appendChild(discardBtn);
     ul.appendChild(li);
 
-    gifName.value = '';
-
-    const buttons = document.querySelectorAll('.card ul');
-
-    Array.from(buttons).map(button => {
-      button.onclick = function(e) {
-        const currentGift = e.target.parentNode.firstChild.nodeValue;
-        let index = 2;
-        if (e.target.textContent === 'Discard') {
-            index = 3;
-        }
-
-        const listGifts = document.querySelectorAll('.card')[index];
-        const li = document.createElement('li');
-        li.textContent = currentGift
-        e.target.parentNode.remove();
-        listGifts.children[1].appendChild(li);
-      };
-    });
+    giftName.value = '';
   });
+
+  function sendButton() {
+      const sendButtons = document.querySelectorAll('button#sendButton');
+      Array.from(sendButtons).forEach(button => {
+         if (this == button){
+           const li = this.parentNode;
+           const sendGiftSection = document.querySelectorAll('.card')[2];
+           const ulSendGift = sendGiftSection.querySelector('ul');
+
+           const newLi = createHTMLElement('li', li.textContent, 'gift');
+           li.remove();
+           ulSendGift.appendChild(newLi);
+         }
+      });
+  }
+
+  function discardButton() {
+     const discardButtons = document.querySelectorAll('button#discardButton');
+     Array.from(discardButtons).forEach(button => {
+        if (this === button) {
+          const li = this.parentNode;
+          const discardGiftSection = document.querySelectorAll('.card')[3];
+          const ulDiscardGift = discardGiftSection.querySelector('ul');
+         
+          const newLi = createHTMLElement('li', li.textContent, 'gift');
+          li.remove();
+          ulDiscardGift.appendChild(newLi);
+        }
+     });
+  }
+
+  function createHTMLElement(tagName, textContent, className, attributes) {
+    const element = document.createElement(tagName);
+
+    if (textContent) {
+      element.textContent = textContent;
+    }
+
+    if (className) {
+      element.className = className;
+    }
+
+    if (attributes) {
+      Object.keys(attributes).forEach(key => {
+          const value = attributes[key];
+          element.setAttribute(key, value);
+      });
+    }
+    return element;
+  }
 }
