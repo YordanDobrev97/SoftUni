31282a6acef3698cf8ab4c68ab058c2ea5490308
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-03-21 00:52:30 +0200

    Templating Exercise - Monkeys

---
+++ JS Advanced/JS Applications/16.Exercise - Templating/07. JS-Applications-Templating-Exercise-Resources/03. Popular Monkeys/monkeysTemplate.js
@@ -0,0 +1,36 @@
+async function monkeyTemplate() {
+    const baseUrl = 'https://monkeys-e457d.firebaseio.com/.json';
+
+    const monkeysDb = await (await fetch(baseUrl)).json();
+    monkeysDb.shift(); // remove first element because he is null
+
+    monkeysDb.forEach(monkey => {
+        monkey.image = `./images/${monkey.image}`;
+    });
+    
+    const templateSource = await (await fetch('./templates/monkey-template.hbs')).text();
+    
+    monkeysDb.forEach(monkey => {
+        const template = Handlebars.compile(templateSource);
+        const result = template(monkey);
+    
+        document.querySelector('section').innerHTML += result;
+    });
+
+    document.querySelectorAll('button').forEach(button => {
+        button.addEventListener('click', showInfo);
+    })
+
+    function showInfo() {
+        const parent = this.parentNode;
+        const p = parent.querySelector('p');
+        
+        if (p.style.display === 'block') {
+            p.style.display = 'none';
+        } else {
+            p.style.display = 'block';
+        }
+    }
+}
+
+monkeyTemplate();
