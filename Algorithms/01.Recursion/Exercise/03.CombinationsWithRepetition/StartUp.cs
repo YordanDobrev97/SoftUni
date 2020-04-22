134dcbcc43204b5d5fd5d490a91563693411d364
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-04-23 00:19:52 +0300

    Algorithms - Combinations with Repetition

---
+++ Algorithms/01.Recursion/Exercise/03.CombinationsWithRepetition/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CombinationsWithRepetition
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int index = 0;
+            int initialValue = 1;
+            int[] array = new int[k];
+
+            GenerateCombinationWithRepetition(index, array, n, initialValue);
+        }
+
+        public static void GenerateCombinationWithRepetition(int index, int[] array, int n, int value)
+        {
+            if (index >= array.Length)
+            {
+                Console.WriteLine(string.Join(" ", array));
+                return;
+            }
+
+            for (int i = value; i <= n; i++)
+            {
+                array[index] = i;
+                GenerateCombinationWithRepetition(index + 1, array, n, i);
+            }
+        }
+    }
+}
