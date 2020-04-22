2feeeadd5fe1f1c126fada4d0a75d8ba964b04f2
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-04-23 00:34:01 +0300

    Algorithms - Combinations without Repetition

---
+++ Algorithms/01.Recursion/Exercise/05.CombinationsWithoutRepetition/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CombinationsWithoutRepetition
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int index = 0;
+            int initialValue = 1;
+            int[] array = new int[k];
+
+            GenerateCombinationWithoutRepetition(index, array, n, initialValue);
+        }
+
+        private static void GenerateCombinationWithoutRepetition(int index, int[] array, int n, int value)
+        {
+            if (index >= array.Length)
+            {
+                Console.WriteLine(string.Join(" ", array));
+                return;
+            }
+
+            for (int i = value; i <= n; i++)
+            {
+                array[index] = i;
+                GenerateCombinationWithoutRepetition(index + 1, array, n, i + 1);
+            }
+        }
+    }
+}
