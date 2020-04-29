b5b1c7370aff5e5fb404e5e489efd83b99c73767
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-04-29 16:36:58 +0300

    Impement Shuffle Algorithm

--- Algorithms/02.Sorting Algoriithms/Lab/04.FisherYatesShuffle/FisherYatesShuffle.cs
+++ Algorithms/02.Sorting Algoriithms/Lab/04.FisherYatesShuffle/FisherYatesShuffle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _04.FisherYatesShuffle
 {
@@ -6,20 +7,27 @@ namespace _04.FisherYatesShuffle
     {
         static void Main()
         {
-            int[] numbers = { 1,2,3,4,5,6 };
+            Console.WriteLine("Read numbers:");
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine("Read something strings:");
+            string[] strings = Console.ReadLine().Split();
+            
             Shuffle(numbers);
+            Shuffle(strings);
 
             Console.WriteLine(string.Join(" ", numbers));
+            Console.WriteLine(string.Join(" ", strings));
         }
 
-        public static void Shuffle(int[] numbers)
+        public static void Shuffle<T>(T[] numbers)
         {
             Random random = new Random();
 
             for (int i = 0; i < numbers.Length; i++)
             {
                 int randomElementIndex = random.Next(i, numbers.Length);
-                int temp = numbers[i];
+                T temp = numbers[i];
                 numbers[i] = numbers[randomElementIndex];
                 numbers[randomElementIndex] = temp;
             }
