3a91ed00bf11b29241408c1179b7063ab8d08da5
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-05-01 20:12:15 +0300

    Implement Permutations Without Repetitions

---
+++ Algorithms/03.Combinatorial Algorithms/Lab/01.PermutationsWithoutRepetitions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01.PermutationsWithoutRepetitions
+{
+    public class Program
+    {
+        private static string[] elements;
+
+        public static void Main()
+        {
+            elements = Console.ReadLine().Split();
+            
+            Permute(0);
+        }
+
+        public static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            Permute(index + 1);
+            for (int i = index + 1; i < elements.Length; i++)
+            {
+                Swap(index, i);
+                Permute(index + 1);
+                Swap(index, i);
+            }
+        }
+
+        private static void Swap(int index, int i)
+        {
+            string temp = elements[index];
+            elements[index] = elements[i];
+            elements[i] = temp;
+        }
+    }
+}
