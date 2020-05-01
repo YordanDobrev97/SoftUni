913ba9114d8e669a21f1132a9e50ca899905ae24
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-05-01 23:43:26 +0300

    Combinatorial Algorithms Lab Exercise - N Choose K Count

---
+++ Algorithms/03.Combinatorial Algorithms/Lab/07.NChooseKCount/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace _07.NChooseKCount
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            BigInteger result = Factorial(n) / (Factorial(n - k) * Factorial(k));
+            Console.WriteLine(result);
+        }
+
+        public static BigInteger Factorial(int num)
+        {
+            BigInteger result = 1;
+
+            for (int i = 2; i <= num; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }       
+}
