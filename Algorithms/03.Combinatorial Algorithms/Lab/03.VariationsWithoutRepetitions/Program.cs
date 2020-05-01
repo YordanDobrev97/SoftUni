ed206bdf134dd241a35db91e544747b75be0d562
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-05-01 22:29:39 +0300

    Impelement Variations Without Repetitions

---
+++ Algorithms/03.Combinatorial Algorithms/Lab/03.VariationsWithoutRepetitions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _03.VariationsWithoutRepetitions
+{
+    public class Program
+    {
+        private static string[] elements;
+        private static string[] variations;
+        private static bool[] used;
+        private static int countVariations;
+
+        public static void Main()
+        {
+            elements = Console.ReadLine().Split();
+            countVariations = int.Parse(Console.ReadLine());
+
+            variations = new string[countVariations];
+            used = new bool[elements.Length];
+            Variations(0);
+        }
+
+        public static void Variations(int index)
+        {
+            if (index >= countVariations)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    variations[index] = elements[i];
+                    Variations(index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
