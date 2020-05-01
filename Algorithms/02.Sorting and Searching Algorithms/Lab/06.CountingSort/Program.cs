632d66958b3e546e52aab4c31ada010ac3152a9d
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-05-01 19:40:45 +0300

    Merge Sorting and Searching Algorithms in one folder

---
+++ Algorithms/02.Sorting and Searching Algorithms/Lab/06.CountingSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.CountingSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 3, 4, 2, 3, 2, 4};
+
+            int min = 0;
+            int max = 10;
+            int[] sorted = Counting(numbers, min, max);
+
+            Console.WriteLine(string.Join(" ", sorted));
+        }
+
+        public static int[] Counting(int[] numbers, int min, int max)
+        {
+
+
+            return null;
+        }
+    }
+}
