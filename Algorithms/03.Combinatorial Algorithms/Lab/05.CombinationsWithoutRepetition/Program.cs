3e21aa6f59abf37467323e8461afea4811547356
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-05-01 22:52:54 +0300

    Implement Combinations Without Repetition

---
+++ Algorithms/03.Combinatorial Algorithms/Lab/05.CombinationsWithoutRepetition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05.CombinationsWithoutRepetition
+{
+    public class Program
+    {
+        private static string[] elements;
+        private static string[] combinations;
+        private static int countCombinations;
+
+        public static void Main()
+        {
+            elements = Console.ReadLine().Split();
+            countCombinations = int.Parse(Console.ReadLine());
+            combinations = new string[countCombinations];
+
+            Combinations(0, 0);
+        }
+
+        public static void Combinations(int index, int start)
+        {
+            if (index >= countCombinations)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = start; i < elements.Length; i++)
+            {
+                combinations[index] = elements[i];
+                Combinations(index + 1, i + 1);
+            }
+        }
+    }
+}
