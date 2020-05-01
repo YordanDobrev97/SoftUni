2a7a73387926cf7d725f22f6748b0d80bdf60de5
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-05-01 22:41:30 +0300

    Implement Variations With Repetition

---
+++ Algorithms/03.Combinatorial Algorithms/Lab/04.VariationsWithRepetition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04.VariationsWithRepetition
+{
+    public class Program
+    {
+        private static string[] elements;
+        private static string[] variations;
+        private static int countVariations;
+
+        public static void Main()
+        {
+            elements = Console.ReadLine().Split();
+            countVariations = int.Parse(Console.ReadLine());
+
+            variations = new string[countVariations];
+            Variations(0);
+        }
+
+        public static void Variations(int index)
+        {
+            if (index >= countVariations)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                variations[index] = elements[i];
+                Variations(index + 1);
+            }
+        }
+    }
+}
