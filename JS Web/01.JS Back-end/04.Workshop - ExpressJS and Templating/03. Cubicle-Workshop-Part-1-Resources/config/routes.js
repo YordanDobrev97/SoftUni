25d74bff97d503ec4d8da533656e968cd4069bf8
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-05-09 21:55:38 +0300

    Move data in database.json file

--- JS Web/01.JS Back-end/04.Workshop - ExpressJS and Templating/03. Cubicle-Workshop-Part-1-Resources/config/routes.js
+++ JS Web/01.JS Back-end/04.Workshop - ExpressJS and Templating/03. Cubicle-Workshop-Part-1-Resources/config/routes.js
@@ -1,9 +1,10 @@
 // TODO: Require Controllers...
 const controllers = require('../controllers/index.js');
+const cubes = require('./database.json');
 
 module.exports = (app) => {
     app.get('/', (req, res) => {
-        res.render('index', {layout: false});
+        res.render('index', {layout: false, cubes: cubes});
     });
 
     app.get('/create', (req, res) => {
