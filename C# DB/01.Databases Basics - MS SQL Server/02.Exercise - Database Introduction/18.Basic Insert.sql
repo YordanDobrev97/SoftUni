95531f33e92d09eb12f0689ac24201ce17a64695
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-05-12 01:06:12 +0300

    All done exercise Database Introduction

---
+++ C# DB/01.Databases Basics - MS SQL Server/02.Exercise - Database Introduction/18.Basic Insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO Towns ([Name]) VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas')
+
+INSERT INTO Departments ([Name]) VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance')
+
+INSERT INTO Employees (FirstName, MiddleName, LastName, JobTitle, DepartmentId, HireDate, Salary, AddressId) VALUES
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '01-02-2013', 950, 1),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '02-03-2004', 900, 2),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '28-08-2016', 525, 3),
+('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '09-12-2007', 900, 4),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '28-08-2016', 599, 1)
---
+++ C# DB/01.Databases Basics - MS SQL Server/02.Exercise - Database Introduction/23.Decrease Tax Rate.sql
@@ -0,0 +1,3 @@
+UPDATE Payments
+SET TaxRate -= TaxRate * 0.03
+SELECT TaxRate FROM Payments
