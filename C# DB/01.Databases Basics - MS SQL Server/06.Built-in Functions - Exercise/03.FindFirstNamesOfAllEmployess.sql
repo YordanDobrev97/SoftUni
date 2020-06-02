3fa8e848b424dec825f198a43046fef2fd496f35
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-06-02 13:47:12 +0300

    Built-in Functions Exercise done all

--- C# DB/01.Databases Basics - MS SQL Server/06.Built-in Functions - Exercise/03.FindFirstNamesOfAllEmployess.sql
+++ C# DB/01.Databases Basics - MS SQL Server/06.Built-in Functions - Exercise/03.FindFirstNamesOfAllEmployess.sql
@@ -1,4 +1,8 @@
-SELECT [FirstName] FROM Employees
-WHERE DepartmentID IN (3,10) AND 
+SELECT [FirstName] 
+FROM Employees
+WHERE DepartmentID IN (3,10) AND
 CAST(DATEPART(year, HireDate) AS INT) 
 BETWEEN 1995 AND 2005
+
+--SELECT FirstName FROM Employees
+--WHERE DepartmentID IN (3, 10) AND (YEAR(HireDate) BETWEEN 1995 AND 2005)
--- C# DB/01.Databases Basics - MS SQL Server/06.Built-in Functions - Exercise/12.CountriesHolding A.sql
+++ C# DB/01.Databases Basics - MS SQL Server/06.Built-in Functions - Exercise/12.CountriesHolding A.sql
@@ -1,3 +1,8 @@
 SELECT [CountryName], IsoCode FROM Countries
 WHERE CountryName LIKE '%a%a%a%'
 ORDER BY IsoCode
+
+SELECT CountryName, IsoCode
+FROM Countries
+WHERE LEN(CountryName) - LEN(REPLACE(CountryName, 'A', '')) >= 3
+ORDER BY IsoCode
