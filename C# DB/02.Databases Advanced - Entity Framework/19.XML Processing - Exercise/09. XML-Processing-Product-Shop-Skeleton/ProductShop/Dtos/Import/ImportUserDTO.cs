3684338d618fb0bc8186356cf1a26a382c123d7d
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-07-21 17:41:13 +0300

    XML Processing - Exercise Import Users

---
+++ C# DB/02.Databases Advanced - Entity Framework/19.XML Processing - Exercise/09. XML-Processing-Product-Shop-Skeleton/ProductShop/Dtos/Import/ImportUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Import
+{
+    [XmlType("User")]
+    public class ImportUserDTO
+    {
+        [XmlElement(ElementName = "firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement(ElementName = "lastName")]
+        public string LastName { get; set; }
+
+        [XmlElement(ElementName = "age")]
+        public int Age { get; set; }
+    }
+}
---
+++ C# DB/02.Databases Advanced - Entity Framework/19.XML Processing - Exercise/09. XML-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        private const string Path = "../../../Datasets";
+
+        public static void Main()
+        {
+            var db = new ProductShopContext();
+            Mapper.Initialize(cng => cng.AddProfile(new ProductShopProfile()));
+            var users = File.ReadAllText(Path + "/users.xml");
+            var result = ImportUsers(db, users);
+            Console.WriteLine(result);
+
+            //EnsureCreatedDatabase(db);
+        }
+
+        public static void EnsureCreatedDatabase(ProductShopContext db)
+        {
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            Console.WriteLine("Successfully created!");
+        }
+
+        //01.ImportUsers
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            var root = new XmlRootAttribute("Users");
+            var serializer = new XmlSerializer(typeof(ImportUserDTO[]), root);
+
+            using (var reader = new StringReader(inputXml))
+            {
+                var users = (ImportUserDTO[])serializer.Deserialize(reader);
+
+                foreach (var userDTO in users)
+                {
+                    var user = Mapper.Map<User>(userDTO);
+                    context.Users.Add(user);
+                }
+            }
+
+            int usersCount = context.SaveChanges();
+
+            return $"Successfully imported {usersCount}";
+        }
+    }
+}
