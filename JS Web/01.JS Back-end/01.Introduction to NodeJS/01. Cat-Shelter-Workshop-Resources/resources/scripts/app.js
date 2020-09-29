ed16168e69f15f3a048a0612bd75320e7020b459
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-09-29 13:10:20 +0300

    Cat-Shelter-Workshop

--- JS Web/01.JS Back-end/01.Introduction to NodeJS/01. Cat-Shelter-Workshop-Resources/resources/scripts/app.js
+++ JS Web/01.JS Back-end/01.Introduction to NodeJS/01. Cat-Shelter-Workshop-Resources/resources/scripts/app.js
@@ -1,28 +1,32 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const router = require('./router');
+const paths = require('./paths');
+const crud = require('./crud');
+const controllers = require('./controllers');
 
 const defaultContentType = 'text/html';
 
 http.createServer((req, res) => {
-    let urlPath = router(req.url);
-    if (!urlPath) {
-        return;
-    }
+    crud.get('/', controllers.home(req, res));
+
+    // let urlPath = paths(req.url);
+    // if (!urlPath) {
+    //     return;
+    // }
     
-    const file = path.join(__dirname, 'public', urlPath);
-    const extName = path.extname(file);
-    let contentType = defaultContentType;
+    // const file = path.join(__dirname, 'public', urlPath);
+    // const extName = path.extname(file);
+    // let contentType = defaultContentType;
 
-    switch (extName) {
-        case '.css': 
-            contentType = 'text/css';
-            break;
-    }
+    // switch (extName) {
+    //     case '.css': 
+    //         contentType = 'text/css';
+    //         break;
+    // }
 
-    res.writeHead(200, {'Content-Type': contentType});
-    const readStream = fs.createReadStream(file);
-    readStream.pipe(res);
+    // res.writeHead(200, {'Content-Type': contentType});
+    // const readStream = fs.createReadStream(file);
+    // readStream.pipe(res);
 
 }).listen(3000, console.log('Server is working at port: 3000'));
