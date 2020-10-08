eb6f35c75874d53b57aa37f4f5b5be66b13abacc
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-10-08 23:17:41 +0300

    Integrated View Engine in MVC Framework

--- C# Web/01.C# Web Basics/Custom MVC Framework/SIS.MvcFramework/Controller.cs
+++ C# Web/01.C# Web Basics/Custom MVC Framework/SIS.MvcFramework/Controller.cs
@@ -1,5 +1,6 @@
 ﻿using SIS.HTTP;
 using SIS.HTTP.Response;
+using SIS.MvcFramework.ViewEngine;
 using System.IO;
 using System.Runtime.CompilerServices;
 
@@ -7,15 +8,28 @@ namespace SIS.MvcFramework
 {
     public abstract class Controller
     {
-        public HttpResponse View([CallerMemberName]string path = null)
+        private SusViewEngine viewEngine;
+
+        public Controller()
+        {
+            this.viewEngine = new SusViewEngine();
+        }
+
+        public HttpResponse View(object viewModel = null,
+            [CallerMemberName]string path = null)
         {
             var layout = File.ReadAllText("Views/Shared/_Layout.cshtml");
-            var fullPath = $"Views/{this.GetType().Name.Replace("Controller", string.Empty)}/{path}.cshtml";
+            layout = layout.Replace("@RenderBody()", "_VIEW_ENGINE_");
+            layout = this.viewEngine.GetHtml(layout, viewModel);
 
+            var fullPath = $"Views/{this.GetType().Name.Replace("Controller", string.Empty)}/{path}.cshtml";
+            
             var html = File.
                 ReadAllText(fullPath);
 
-            layout = layout.Replace("@RenderBody()", html);
+            html = this.viewEngine.GetHtml(html, viewModel);
+
+            layout = layout.Replace("_VIEW_ENGINE_", html);
 
             return new HtmlResponse(layout);
         }
